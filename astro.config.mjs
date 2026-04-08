17e894830ebf9afab4f1639de64da530db433843
Nick <ngolabovski819@gmail.com>
2026-04-09 00:56:36 +0200

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://findbyface.org',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/api/'),
+    }),
+  ],
 });
